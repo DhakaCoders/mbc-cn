5577beb28d2c67bddafe247807c7127989f30e4b
monirulislamweb <65311288+monirulislamweb@users.noreply.github.com>
2021-09-28 16:47:12 +0600

    ..

--- themes/mbc/assets/js/main.js
+++ themes/mbc/assets/js/main.js
@@ -234,7 +234,18 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
     /*start of noyon*/
 
-
+    if( $('.hamburger-cntlr').length ){
+      $('.hamburger-cntlr').click(function(){
+        $('body').toggleClass('allWork');
+      });
+    }
+    if( $('li.menu-item-has-children a').length ){
+      $('li.menu-item-has-children a').click(function(e){
+       event.preventDefault();
+       $(this).next().slideToggle(300);
+       $(this).parent().toggleClass('this-sub-menu-active');
+     });
+    }
 
 
      /*start of shariful*/
